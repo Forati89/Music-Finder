4086f3f15672d506cc19160b9fe4f1441c70c64e
forati89 <angel3r@hotmail.com>
2019-09-19 15:29:15 +0200

    Updates

---
+++ precache-manifest.d754fc87146f1c9a9f77063be98a6ec0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4e049dd3de52b0bacfbb6a715babb3",
+    "url": "/Music-Finder/index.html"
+  },
+  {
+    "revision": "1da5c8d768f248f0d297",
+    "url": "/Music-Finder/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b0240586a5e59b92a80e",
+    "url": "/Music-Finder/static/css/main.56344ff2.chunk.css"
+  },
+  {
+    "revision": "1da5c8d768f248f0d297",
+    "url": "/Music-Finder/static/js/2.eaf3874a.chunk.js"
+  },
+  {
+    "revision": "b0240586a5e59b92a80e",
+    "url": "/Music-Finder/static/js/main.0004388e.chunk.js"
+  },
+  {
+    "revision": "8549d857e1cb41318a76",
+    "url": "/Music-Finder/static/js/runtime~main.917f8457.js"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/Music-Finder/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/Music-Finder/static/media/roboto-condensed.light.cf4449c1.ttf"
+  }
+]);
